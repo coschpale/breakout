08dd16f75d4da67448ed0f7c7ccde078894f82d5
petkosamoletko <ntzotchev@gmail.com>
2023-07-19 12:06:03 +0200

    remove unused code

--- game/montecarlo.py
+++ game/montecarlo.py
@@ -7,34 +7,18 @@ class MCModel:
         self.gamma = gamma
         self.epsilon = epsilon
         self.Q = None
-        if isinstance(action_space, int):
-            self.action_space = np.arange(action_space)
-            actions = [0] * action_space
-            # Action representation
-            self._act_rep = "list"
-        else:
-            self.action_space = action_space
-            actions = {k: 0 for k in action_space}
-            self._act_rep = "dict"
-        if isinstance(state_space, int):
-            self.state_space = np.arange(state_space)
-            self.Q = [deepcopy(actions) for _ in range(state_space)]
-        else:
-            self.state_space = state_space
-            self.Q = {k: deepcopy(actions) for k in state_space}
-
+        self.action_space = np.arange(action_space)
+        actions = [0] * action_space
+        # Action representation
+        self.state_space = np.arange(state_space)
+        self.Q = [deepcopy(actions) for _ in range(state_space)]
         # Frequency of state/action.
         self.Ql = deepcopy(self.Q)
 
     def pi(self, action, state):
-        if self._act_rep == "list":
-            if action == np.argmax(self.Q[state]):
-                return 1
-            return 0
-        elif self._act_rep == "dict":
-            if action == max(self.Q[state], key=self.Q[state].get):
-                return 1
-            return 0
+        if action == np.argmax(self.Q[state]):
+            return 1
+        return 0
 
     def b(self, action, state):
         return self.epsilon / len(self.action_space) + (1 - self.epsilon) * self.pi(action, state)
@@ -60,22 +44,3 @@ class MCModel:
             N = self.Ql[state][action]
             self.Q[state][action] = q * N / (N + 1) + G[s] / (N + 1)
 
-    def score(self, env, policy, n_samples=1000):
-        rewards = []
-        for _ in range(n_samples):
-            observation = env.reset()
-            cum_rewards = 0
-            while True:
-                action = self.choose_action(policy, observation)
-                observation, reward, done, _ = env.step(action)
-                cum_rewards += reward
-                if done:
-                    rewards.append(cum_rewards)
-                    break
-        return np.mean(rewards)
-
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
